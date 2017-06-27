02eaf6ea94dfabfe1e95f0f3189d5fbfd8218fd2
顾世杰 <techel@live.cn>
2017-06-27 17:07:11 -0400

    add matlab

---
+++ BatchVer/BatchVer.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH -n 1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=150000
+#SBATCH -t 0-6:00
+#SBATCH --time-min=0-01:00
+#SBATCH -o /net/feevault/data0/shared/EmilyShijieShared/BatchResultTest/job_%A.out
+#SBATCH -e /net/feevault/data0/shared/EmilyShijieShared/BatchResultTestjob_%A.err
+
+cd /net/feevault/data0/shared/EmilyShijieShared/BatchResultTest/
+module add mit/matlab/2016b
+cmd="matlab -nodisplay -singleCompThread -r"
+mfun=/home/shijiegu/cnmf_e/BatchVer/BatchVerScript
+srun $cmd "$mfun"
