01248040a319eba3f81b22b02ad56d6722f2fa0e
顾世杰 <techel@live.cn>
2017-06-27 20:32:49 -0400

    smaller RAM request!

--- BatchVer/BatchVer.sh
+++ BatchVer/BatchVer.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -n 1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=150000
+#SBATCH --mem=50000
 #SBATCH -t 0-6:00
 #SBATCH --time-min=0-01:00
 #SBATCH -o /net/feevault/data0/shared/EmilyShijieShared/BatchResultTest/job_%A.out
