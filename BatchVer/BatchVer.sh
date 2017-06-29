c7b69bdb2ecea534e48714f97f0e4bd878cf7e5c
顾世杰 <techel@live.cn>
2017-06-29 15:44:50 -0400

    using small data

--- BatchVer/BatchVer.sh
+++ BatchVer/BatchVer.sh
@@ -2,13 +2,13 @@
 
 #SBATCH -n 1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=50000
-#SBATCH -t 0-10:00
+#SBATCH --mem=20000
+#SBATCH -t 0-2:00
 #SBATCH --time-min=0-01:00
-#SBATCH -o /net/feevault/data0/shared/EmilyShijieShared/BatchResult/7030FirstFewDaysForBatch/job_%A.out
-#SBATCH -e /net/feevault/data0/shared/EmilyShijieShared/BatchResult/7030FirstFewDaysForBatch/job_%A.err
+#SBATCH -o /net/feevault/data0/shared/EmilyShijieShared/BatchResultTest/job_%A.out
+#SBATCH -e /net/feevault/data0/shared/EmilyShijieShared/BatchResultTest/job_%A.err
 
-cd /net/feevault/data0/shared/EmilyShijieShared/BatchResult/6938FirstFewDaysForBatch/
+cd /net/feevault/data0/shared/EmilyShijieShared/BatchResultTest/
 module add mit/matlab/2016b
 matlab -nodisplay -singleCompThread -r "addpath(genpath('/home/shijiegu/cnmf_e/')); 
 BatchVerScript"
