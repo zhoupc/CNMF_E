ca29f9d4223cce0258482e927569db1440240ce7
顾世杰 <techel@live.cn>
2017-06-28 23:51:53 -0400

    new data directories in scripts

--- BatchVer/BatchVer.sh
+++ BatchVer/BatchVer.sh
@@ -2,12 +2,12 @@
 
 #SBATCH -n 1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=50000
-#SBATCH -t 0-6:00
+#SBATCH --mem=200000
+#SBATCH -t 0-10:00
 #SBATCH --time-min=0-01:00
-#SBATCH -o /net/feevault/data0/shared/EmilyShijieShared/BatchResultTest/job_%A.out
-#SBATCH -e /net/feevault/data0/shared/EmilyShijieShared/BatchResultTestjob_%A.err
+#SBATCH -o /net/feevault/data0/shared/EmilyShijieShared/BatchResult/7030FirstFewDaysForBatch/job_%A.out
+#SBATCH -e /net/feevault/data0/shared/EmilyShijieShared/BatchResult/7030FirstFewDaysForBatch/job_%A.err
 
-cd /net/feevault/data0/shared/EmilyShijieShared/BatchResultTest/
+cd /net/feevault/data0/shared/EmilyShijieShared/BatchResult/7030FirstFewDaysForBatch/
 module add mit/matlab/2016b
 matlab -nodisplay -singleCompThread -r "addpath(genpath('/home/shijiegu/cnmf_e/')); BatchVerScript"
--- BatchVer/BatchVerScript.m
+++ BatchVer/BatchVerScript.m
@@ -1,3 +1,3 @@
-load(fullfile('/net/feevault/data0/shared/EmilyShijieShared/BatchResultTest/','LogisticscnmfeBatchVer.mat'))
+load(fullfile('/net/feevault/data0/shared/EmilyShijieShared/BatchResult/7030FirstFewDaysForBatch/','LogisticscnmfeBatchVer.mat'))
 addpath(genpath(codeDir));
 cnmfeBatchVer_ClusterPart
