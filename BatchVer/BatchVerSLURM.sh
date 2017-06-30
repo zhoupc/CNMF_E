1d128e9b15a0d3d423e80d311d1a49db6dc5c63b
顾世杰 <techel@live.cn>
2017-06-30 11:23:32 -0400

    more ram

--- BatchVer/BatchVerSLURM.sh
+++ BatchVer/BatchVerSLURM.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -n 1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=100000
+#SBATCH --mem=200000
 #SBATCH -t 0-10:00
 #SBATCH --time-min=0-01:00
 #SBATCH -o /net/feevault/data0/shared/EmilyShijieShared/BatchResult/6922FirstFewDaysForBatch/job_%A.out
