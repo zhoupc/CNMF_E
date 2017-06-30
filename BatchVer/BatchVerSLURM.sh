0b161c2119cc35e39942301dff00ac9536a6fce6
顾世杰 <techel@live.cn>
2017-06-29 21:34:37 -0400

    maybe this empty line is important?

--- BatchVer/BatchVerSLURM.sh
+++ BatchVer/BatchVerSLURM.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+
 #SBATCH -n 1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=100000
+#SBATCH --mem=200000
 #SBATCH -t 0-10:00
 #SBATCH --time-min=0-01:00
-#SBATCH -o /net/feevault/data0/elm/shared/EmilyShijieShared/BatchResult/6922FirstFewDaysForBatch/job_%A.out
-#SBATCH -e /net/feevault/data0/elm/shared/EmilyShijieShared/BatchResult/6922FirstFewDaysForBatch/job_%A.err
-cd /net/feevault/data0/elm/shared/EmilyShijieShared/BatchResult/6922FirstFewDaysForBatch/
+#SBATCH -o /net/feevault/data0/shared/EmilyShijieShared/BatchResult/6922FirstFewDaysForBatch/job_%A.out
+#SBATCH -e /net/feevault/data0/shared/EmilyShijieShared/BatchResult/6922FirstFewDaysForBatch/job_%A.err
+cd /net/feevault/data0/shared/EmilyShijieShared/BatchResult/6922FirstFewDaysForBatch/
 module add mit/matlab/2016b
-matlab -nodisplay -singleCompThread -r "addpath(genpath('/home/elm/CaImagingCode/cnmf_e/'));\
-load(fullfile('/net/feevault/data0/elm/shared/EmilyShijieShared/BatchResult/6922FirstFewDaysForBatch/','LogisticscnmfeBatchVer.mat')); cnmfeBatchVer_ClusterPart "
+matlab -nodisplay -singleCompThread -r "addpath(genpath('/home/shijiegu/cnmf_e/'));\
+load(fullfile('/net/feevault/data0/shared/EmilyShijieShared/BatchResult/6922FirstFewDaysForBatch/','LogisticscnmfeBatchVer.mat')); cnmfeBatchVer_ClusterPart "
