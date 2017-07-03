46c4a6cec38df6c01d3026c297fc8bec34d66b9f
顾世杰 <techel@live.cn>
2017-07-03 15:33:25 -0400

    Trunked version!

--- BatchVer/BatchVerSLURM.sh
+++ BatchVer/BatchVerSLURM.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -n 1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=200000
+#SBATCH --mem=150000
 #SBATCH -t 0-10:00
 #SBATCH --time-min=0-01:00
 #SBATCH -o /net/feevault/data0/shared/EmilyShijieShared/BatchResult/6922FirstFewDaysForBatchNewMerging/job_%A.out
@@ -10,4 +10,4 @@
 cd /net/feevault/data0/shared/EmilyShijieShared/BatchResult/6922FirstFewDaysForBatchNewMerging/
 module add mit/matlab/2016b
 matlab -nodisplay -singleCompThread -r "addpath(genpath('/home/shijiegu/cnmf_e/'));\
-load(fullfile('/net/feevault/data0/shared/EmilyShijieShared/BatchResult/6922FirstFewDaysForBatchNewMerging/','LogisticscnmfeBatchVer.mat')); cnmfeBatchVer_ClusterPart "
+load(fullfile('/net/feevault/data0/shared/EmilyShijieShared/BatchResult/6922FirstFewDaysForBatchNewMerging/','PartOneOFcnmfeBatchVer.mat')); BatchVerTestVer "
