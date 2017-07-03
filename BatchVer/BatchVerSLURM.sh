c3898a2fcb47e9dce9dcdede9c2195b636ee5c9a
顾世杰 <techel@live.cn>
2017-07-03 12:20:29 -0400

    new folder for new merging test

--- BatchVer/BatchVerSLURM.sh
+++ BatchVer/BatchVerSLURM.sh
@@ -2,12 +2,12 @@
 
 #SBATCH -n 1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=150000
+#SBATCH --mem=200000
 #SBATCH -t 0-10:00
 #SBATCH --time-min=0-01:00
-#SBATCH -o /net/feevault/data0/shared/EmilyShijieShared/BatchResult/6922FirstFewDaysForBatchNoNONNeuron/job_%A.out
-#SBATCH -e /net/feevault/data0/shared/EmilyShijieShared/BatchResult/6922FirstFewDaysForBatchNoNONNeuron/job_%A.err
-cd /net/feevault/data0/shared/EmilyShijieShared/BatchResult/6922FirstFewDaysForBatchNoNONNeuron/
+#SBATCH -o /net/feevault/data0/shared/EmilyShijieShared/BatchResult/6922FirstFewDaysForBatchNewMerging/job_%A.out
+#SBATCH -e /net/feevault/data0/shared/EmilyShijieShared/BatchResult/6922FirstFewDaysForBatchNewMerging/job_%A.err
+cd /net/feevault/data0/shared/EmilyShijieShared/BatchResult/6922FirstFewDaysForBatchNewMerging/
 module add mit/matlab/2016b
 matlab -nodisplay -singleCompThread -r "addpath(genpath('/home/shijiegu/cnmf_e/'));\
-load(fullfile('/net/feevault/data0/shared/EmilyShijieShared/BatchResult/6922FirstFewDaysForBatchNoNONNeuron/','LogisticscnmfeBatchVer.mat')); cnmfeBatchVer_ClusterPart "
+load(fullfile('/net/feevault/data0/shared/EmilyShijieShared/BatchResult/6922FirstFewDaysForBatchNewMerging/','LogisticscnmfeBatchVer.mat')); cnmfeBatchVer_ClusterPart "
