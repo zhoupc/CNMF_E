33a60b403509c9d14cfc49082be11eafdd1d376d
顾世杰 <techel@live.cn>
2017-07-26 08:06:47 -0400

    BatchVerMotion Cluster1 is shortened and these cut are put in Cluster2

--- BatchVer/cnmfeBatchVer_ClusterPart.m
+++ BatchVer/cnmfeBatchVer_ClusterPart.m
@@ -28,7 +28,6 @@ if ~isempty(emptyA0s_ind)
     A0s(emptyA0s_ind)=[];    
     File(emptyA0s_ind)=[];
 end
-save([outputdir 'NormalsOFcnmfeBatchVer.mat'],'-v7.3')
 
 %%% Order similar neurons in the same sequence in each file, not necessary,
 %%% but nice to do. It is fast.
@@ -46,45 +45,16 @@ parfor i= 1:S_R
     end
     ACS(i).Ain=Ain; ACS(i).Cin=Cin; ACS(i).STD=STD;
 end
-save([outputdir 'PartOneOFcnmfeBatchVer.mat'],'-v7.3')
-%% 3 Merge similar neurons
-%%% Merge similar neurons based on spatial AND temporal correlation
-% C_all=cat(2,ACS.Cin);
-% Amask_temp=cat(2,A0s{1:2});
-% Amask_temp=bsxfun(@gt,Amask_temp,quantile(Amask_temp,0.3)); %only use central part for merging.
 
-% C_temp=C_all(1:size(Amask_temp,2),:);
-% [Amask_temp,C_temp,ACS] = mergeAC(Amask_temp,C_temp,ACS,merge_thr_2);
-% 
-% merge2start=1+size(A0s{1},2)+size(A0s{2},2);
-% for i=3:length(samplelist)
-%     Amask_temp=cat(2,Amask_temp,A0s{i});
-%     Amask_temp=bsxfun(@gt,Amask_temp,quantile(Amask_temp,0.3));
-%     
-%     C_temp=[C_temp;C_all(merge2start:merge2start+size(A0s{i},2)-1,:)];
-% 
-%     [Amask_temp,C_temp,ACS] = mergeAC(Amask_temp,C_temp,ACS,merge_thr_2);
-%     merge2start=merge2start+size(A0s{i},2);
-% end
+%% 3 Merge similar neurons
 
 Amask_temp=cat(2,A0s{:});
 Amask_temp=bsxfun(@gt,Amask_temp,quantile(Amask_temp,0.3)); %only use central part for merging.
-%C_all=cat(2,ACS.Cin);
-
 [Afinal,MC,newIDs,merged_ROIs] = mergeAC(Amask_temp,ACS,merge_thr_2);
-% [size1,~]=cellfun(@size,newIDs);
-% ACS(size1~=1)
+
 save([outputdir 'commonAcnmfeBatchVer.mat'],'-v7.3')
-%% 4 Collapsing A's
-% As=cell(1,length(samplelist));
-% STDs=cell(1,length(samplelist));
-% parfor i=1:length(samplelist)
-%     As{i}=ACS(i).Ain;
-%     STDs{i}=ACS(i).STD;    
-% end
-% Afinal=ReducingA(As,STDs); % the format for cell input is designed for potential other applications.
-% save([outputdir 'uniqueAcnmfeBatchVer.mat'],'-v7.3')
-%% 4.5 Determine Afinal that will be used to extract C's in each file.
+
+%% 4 Determine Afinal that will be used to extract C's in each file.
 
 %%% Some processes making Afinal nicer, modified from Pengcheng Zhou's
 %%% idea.
@@ -103,7 +73,9 @@ newIDs=newIDs(nz_ind);
 
 Apicname=sprintf('%.0fAfinal',daynum);
 ColorAllNeurons(Afinal,File(1).options.d1,File(2).options.d2,Apicname,Aoutputdir);
-eval(sprintf('save %s%.0fAfinalcnmfeBatchVer %s', Aoutputdir,daynum, 'Afinal'));
+
+Vars = {'Afinal';'samplelist';'File'}; Vars=Vars';
+eval(sprintf('save %s%0.f_cnmfe_BatchVer_ClusterPartI.mat %s -v7.3', Aoutputdir, daynum, strjoin(Vars)));
 %% 5 "massive" procedure: Extract A from each file
 neuron_batch(length(filelist)) = struct('ind_del',[],'signal',[],'FileOrigin',[],'neuron',[]);
 
@@ -113,19 +85,11 @@ parfor i= 1:length(filelist)
     [~,neuron_batch(i)]=demo_endoscope2(gSig,gSiz,min_corr,min_pnr,min_pixel,bd,FS,SSub,TSub,bg_neuron_ratio,nam,mode,[],Afinal,neuron_batch(i),convolveType,merge_thr);
     neuron_batch(i).FileOrigin=filelist(i); % save origin(filelist)
 end
-fprintf('Massive extraction done.');
-save([outputdir 'MassivecnmfeBatchVer.mat'],'-v7.3')
+fprintf('Massive extraction in each file done.');
 
 %% 6 Partition between those neurons found in each file and those not. Save results.
-% ind_del_final_cat=cat(2,neuron_batch.ind_del);
-% ind_del_final=any(ind_del_final_cat,2);
+
 parfor i= 1:length(filelist)
-%     neuron_batch(i).neuron.A=[neuron_batch(i).neuron.A(:,~ind_del_final) neuron_batch(i).neuron.A(:,ind_del_final)];
-%     fprintf('A extraction done\n');
-%     neuron_batch(i).neuron.C=[neuron_batch(i).neuron.C(~ind_del_final,:);neuron_batch(i).neuron.C(ind_del_final,:)];
-%     neuron_batch(i).neuron.C_raw=[neuron_batch(i).neuron.C_raw(~ind_del_final,:);neuron_batch(i).neuron.C_raw(ind_del_final,:)];
-%     fprintf('C extraction done\n');
-            %%% save each data i's signal into neuron(i).signal and
     for j=1:size(neuron_batch(i).neuron.A,2)
         jA=neuron_batch(i).neuron.A(:,j);
         jC=neuron_batch(i).neuron.C(j,:);
