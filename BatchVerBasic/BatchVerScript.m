c6aca9dd3cf87f79762a546c87668c9a23e15169
顾世杰 <techel@live.cn>
2017-07-27 15:36:14 -0400

    BatchVerBasic

--- BatchVerBasic/BatchVerScript.m
+++
@@ -1,3 +0,0 @@
-load(fullfile('/net/feevault/data0/shared/EmilyShijieShared/BatchResult/7030FirstFewDaysForBatch/','LogisticscnmfeBatchVer.mat'))
-addpath(genpath(codeDir));
-cnmfeBatchVer_ClusterPart
