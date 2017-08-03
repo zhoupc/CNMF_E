67a575aca7598a209d05fffca382d6fdb36509ad
ShijieGu <techel@live.cn>
2017-08-03 18:36:31 -0400

    motionGUI

---
+++ BatchVerMO/motion/ManualShift.m
@@ -0,0 +1,244 @@
+function varargout = ManualShift(varargin)
+% MANUALSHIFT MATLAB code for ManualShift.fig
+%      MANUALSHIFT, by itself, creates a new MANUALSHIFT or raises the existing
+%      singleton*.
+%
+%      H = MANUALSHIFT returns the handle to a new MANUALSHIFT or the handle to
+%      the existing singleton*.
+%
+%      MANUALSHIFT('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in MANUALSHIFT.M with the given input arguments.
+%
+%      MANUALSHIFT('Property','Value',...) creates a new MANUALSHIFT or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before ManualShift_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to ManualShift_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help ManualShift
+
+% Last Modified by GUIDE v2.5 03-Aug-2017 18:14:49
+
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @ManualShift_OpeningFcn, ...
+                   'gui_OutputFcn',  @ManualShift_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before ManualShift is made visible.
+function ManualShift_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to ManualShift (see VARARGIN)
+
+% Parsing inputs
+p = inputParser;
+addParameter(p,'M',[]);
+parse(p, varargin{:});
+handles.M=p.Results.M; M=p.Results.M;
+
+% Creat the overlay picture
+A=A2image(M{2}{2},300,400);
+B=A2image(M{2}{3},300,400);
+imshowpair(A,B,'falsecolor','Scaling','independent');
+
+axes(handles.axes2)
+imshow(A)
+
+axes(handles.axes3)
+imshow(B)
+
+% Choose default command line output for ManualShift
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes ManualShift wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = ManualShift_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+
+function TemplateChoice_Callback(hObject, eventdata, handles)
+% hObject    handle to TemplateChoice (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of TemplateChoice as text
+%        str2double(get(hObject,'String')) returns contents of TemplateChoice as a double
+cuurent_template=get(hObject,'String');
+
+
+% --- Executes during object creation, after setting all properties.
+function TemplateChoice_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to TemplateChoice (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+function neuron_list_tmpl_Callback(hObject, eventdata, handles)
+% hObject    handle to neuron_list_tmpl (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of neuron_list_tmpl as text
+%        str2double(get(hObject,'String')) returns contents of neuron_list_tmpl as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function neuron_list_tmpl_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to neuron_list_tmpl (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function neuron_list_2align_Callback(hObject, eventdata, handles)
+% hObject    handle to neuron_list_2align (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of neuron_list_2align as text
+%        str2double(get(hObject,'String')) returns contents of neuron_list_2align as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function neuron_list_2align_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to neuron_list_2align (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on slider movement.
+function slider1_Callback(hObject, eventdata, handles)
+% hObject    handle to slider1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+
+
+% --- Executes during object creation, after setting all properties.
+function slider1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to slider1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
+
+
+
+function ToAlign_Callback(hObject, eventdata, handles)
+% hObject    handle to ToAlign (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of ToAlign as text
+%        str2double(get(hObject,'String')) returns contents of ToAlign as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function ToAlign_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to ToAlign (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in pushbutton3.
+function pushbutton3_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes during object creation, after setting all properties.
+function axes1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to axes1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: place code in OpeningFcn to populate axes1
+
+
+% --- Executes during object creation, after setting all properties.
+function axes2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to axes2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: place code in OpeningFcn to populate axes2
